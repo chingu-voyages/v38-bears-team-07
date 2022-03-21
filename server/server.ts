becc0ef81f28796b3dbf05d1ce38be8468e10267
Josue Rodriguez <maybejosue@gmail.com>
2022-03-21 11:44:45 -0700

    Adds custom express error handler

--- server/server.ts
+++ server/server.ts
@@ -1,32 +1,28 @@
 import express, { Application, Request, Response } from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import cors from 'cors'
-import authRoute from './routes/authRoute'
+import process from 'process';
+import cors from 'cors';
+import authRoute from './routes/authRoute';
+import errorHandler from './middleware/error';
 
-dotenv.config()
+dotenv.config();
 
 const app: Application = express();
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
-const mongoKey = `${process.env.MONGO_URI}`;
+mongoose.connect(`${process.env.MONGO_URI}`);
 
-mongoose.connect(
-    mongoKey,
-    () => {
-        console.log('connected to db!')
-    }
-);
+app.use('/auth', authRoute);
+app.use(errorHandler);
 
-app.use("/auth", authRoute)
-
-app.get("/", (req: Request, res: Response) => {
-   res.send('hello world')
-})
+app.get('/', (req: Request, res: Response) => {
+    res.send('API is running');
+});
 
 const PORT = process.env.PORT || 5002;
 
 app.listen(PORT, () => {
-    console.log(`Server running in PORT:${PORT}`)
-})
+    console.log(`Server running in PORT:${PORT}`);
+});
